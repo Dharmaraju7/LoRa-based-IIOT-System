ffa89fe8be123587ab8d5d3b074b72e77f537564
Dharmaraju7 <78192012+Dharmaraju7@users.noreply.github.com>
2021-01-29 10:55:30 +0530

    Add files via upload

---
+++ Fipy/Fipy-Fipy/Fipy-B/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, Pycom Limited.
+#
+# This software is licensed under the GNU GPL version 3 or any
+# later version, with permitted additional terms. For more information
+# see the Pycom Licence v1.0 document supplied with this file, or
+# available at https://www.pycom.io/opensource/licensing
+#
+
+from network import LoRa
+import socket
+import time
+
+# Please pick the region that matches where you are using the device
+
+lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+s.setblocking(False)
+i = 0
+while True:
+    if s.recv(64) == b'Ping':
+        s.send('Pong')
+        print('Pong {}'.format(i))
+        i = i+1
+    time.sleep(5)
