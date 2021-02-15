70c12562fb94bdbd97bf88aa82336cd59a14761f
CoderMohitSinha <mohitsinha1994@gmail.com>
2021-02-15 09:08:32 +0530

    date update

---
+++ pytrack/date.py
@@ -0,0 +1,11 @@
+# Python program to get 
+# current date 
+
+
+# Import datetime class from datetime module 
+from datetime import datetime 
+
+
+# returns current date and time 
+now = datetime.now() 
+print("now = ", now) 
